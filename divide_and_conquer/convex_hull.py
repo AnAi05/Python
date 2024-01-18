f128501f4943172de6ad0e925b52985403fcc91e
Omkar Jajulwar <omkarjajulwar@gmail.com>
2024-01-18 12:33:49 +0000

    added jarvis algorithm in divide_and_conquer/convex_hull.py

--- divide_and_conquer/convex_hull.py
+++ divide_and_conquer/convex_hull.py
@@ -476,19 +476,104 @@ def convex_hull_melkman(points: list[Point]) -> list[Point]:
     # `convex_hull` is contains the convex hull in circular order
     return sorted(convex_hull[1:] if len(convex_hull) > 3 else convex_hull)
 
+def Left_index(points): 
+      
+    ''' 
+    Finding the left most point 
+    '''
+    minn = 0
+    for i in range(1,len(points)): 
+        if points[i].x < points[minn].x: 
+            minn = i 
+        elif points[i].x == points[minn].x: 
+            if points[i].y > points[minn].y: 
+                minn = i 
+    return minn 
+  
+def orientation(p, q, r): 
+    ''' 
+    To find orientation of ordered triplet (p, q, r).  
+    The function returns following values  
+    0 --> p, q and r are collinear  
+    1 --> Clockwise  
+    2 --> Counterclockwise  
+    '''
+    val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y) 
+  
+    if val == 0: 
+        return 0
+    elif val > 0: 
+        return 1
+    else: 
+        return 2
+  
+def convex_hull_jarvis(points): 
+      
+    # There must be at least 3 points  
+    n = len(points)
+    points = sorted(_validate_input(points))
+  
+    # Find the leftmost point 
+    l = Left_index(points) 
+  
+    hull = [] 
+      
+    ''' 
+    Start from leftmost point, keep moving counterclockwise  
+    until reach the start point again. This loop runs O(h)  
+    times where h is number of points in result or output.  
+    '''
+    p = l 
+    q = 0
+    while(True): 
+          
+        # Add current point to result  
+        hull.append(p) 
+  
+        ''' 
+        Search for a point 'q' such that orientation(p, q,  
+        x) is counterclockwise for all points 'x'. The idea  
+        is to keep track of last visited most counterclock-  
+        wise point in q. If any point 'i' is more counterclock-  
+        wise than q, then update q.  
+        '''
+        q = (p + 1) % n 
+  
+        for i in range(n): 
+              
+            # If i is more counterclockwise  
+            # than current q, then update q  
+            if(orientation(points[p],  
+                           points[i], points[q]) == 2): 
+                q = i 
+  
+        ''' 
+        Now q is the most counterclockwise with respect to p  
+        Set p as q for next iteration, so that q is added to  
+        result 'hull'  
+        '''
+        p = q 
+  
+        # While we don't come to first point 
+        if(p == l): 
+            break
+  
+    # Print Result  
+    hull = sorted(hull)
+    return [points[point] for point in hull]
 
 def main():
     points = [
-        (0, 3),
-        (2, 2),
-        (1, 1),
-        (2, 1),
-        (3, 0),
-        (0, 0),
-        (3, 3),
-        (2, -1),
-        (2, -4),
-        (1, -3),
+        Point(0, 3),
+        Point(2, 2),
+        Point(1, 1),
+        Point(2, 1),
+        Point(3, 0),
+        Point(0, 0),
+        Point(3, 3),
+        Point(2, -1),
+        Point(2, -4),
+        Point(1, -3),
     ]
     # the convex set of points is
     # [(0, 0), (0, 3), (1, -3), (2, -4), (3, 0), (3, 3)]
@@ -499,6 +584,9 @@ def main():
 
     results_melkman = convex_hull_melkman(points)
     assert results_bf == results_melkman
+
+    results_jarvis = convex_hull_jarvis(points)
+    assert results_bf == results_jarvis
 
     print(results_bf)
 
